d7f8a2d8e951fff1336b01fc8c8e6b713def9840
SupakornIm <supakorn.imjai@gmail.com>
2023-08-03 21:11:43 +0700

    style: update contact link to button

--- src/components/sub-components/Contact.jsx
+++ src/components/sub-components/Contact.jsx
@@ -4,46 +4,44 @@ import "./Contact.css";
 function Contact() {
   return (
     <div className="contact-container">
-
-      <div className="my-contact">
-        <div>
-          <img className="contact-image" src=".\src\assets\Vector.png" alt="" />
-          <img className="github" src=".\src\assets\github.svg" alt="" />
+      <a href="https://github.com/supakorntaee">
+        <div className="my-contact">
+          <div>
+            <img className="contact-image" src=".\src\assets\Vector.png" alt="" />
+            <img className="github" src=".\src\assets\github.svg" alt="" />
+          </div>
+          <h3 className="contact-platform">Github</h3>
         </div>
-        <h3 className="contact-platform">Github</h3>
-      </div>
+      </a>
 
-
-
-      <div className="my-contact">
-        <div>
-          <img className="contact-image" src=".\src\assets\Vector.png" alt="" />
-          <img className="instagram" src=".\src\assets\Instagram.svg" alt="" />
+      <a href="https://www.instagram.com/">
+        <div className="my-contact">
+          <div>
+            <img className="contact-image" src=".\src\assets\Vector.png" alt="" />
+            <img className="instagram" src=".\src\assets\Instagram.svg" alt="" />
+          </div>
+          <h3 className="contact-platform">Instagram</h3>
         </div>
-        <h3 className="contact-platform">Instagram</h3>
-      </div>
-
-
-
-      <div className="my-contact">
-        <div>
-          <img className="contact-image" src=".\src\assets\Vector.png" alt="" />
-          <img className="email" src=".\src\assets\fast message.png" alt="" />
+      </a>
+      <a href="mailto:your.email@example.com">
+        <div className="my-contact">
+          <div>
+            <img className="contact-image" src=".\src\assets\Vector.png" alt="" />
+            <img className="email" src=".\src\assets\fast message.png" alt="" />
+          </div>
+          <h3 className="contact-platform">Email</h3>
         </div>
-        <h3 className="contact-platform">Email</h3>
-      </div>
+      </a>
 
-
-
-      <div className="my-contact">
-        <div>
-          <img className="contact-image" src=".\src\assets\Vector.png" alt="" />
-          <img className="call" src=".\src\assets\call.png" alt="" />
+      <a href="tel:+1234567890">
+        <div className="my-contact">
+          <div>
+            <img className="contact-image" src=".\src\assets\Vector.png" alt="" />
+            <img className="call" src=".\src\assets\call.png" alt="" />
+          </div>
+          <h3 className="contact-platform">Telp</h3>
         </div>
-        <h3 className="contact-platform">Telp</h3>
-      </div>
-
-
+      </a>
     </div>
   );
 }
